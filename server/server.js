17fe5c1f3b9e24ab5cfc3149fb73b7cc9e16c6ed
Priyanka1593 <priya.patgaokar@gmail.com>
2017-12-21 15:48:30 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express=require('express');
 const path=require('path');
+const http=require('http');
+const socketIO=require('socket.io');
 const publicPath=path.join(__dirname,'../public');
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 var port=process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+
+    socket.on('disconnect',()=>{
+        console.log('Client was Disconnected');
+    })
+});
+
+server.listen(port,()=>{
     console.log(`connected to port ${port}`);
 });
